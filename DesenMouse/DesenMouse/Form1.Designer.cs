e646d32b37cfb3e9212966df97e12257f898e48e
newparts <iuliu@newparts.ro>
2018-11-30 17:18:40 +0200

    Desen Final
    desen cu mouse final

--- DesenMouse/DesenMouse/Form1.Designer.cs
+++ DesenMouse/DesenMouse/Form1.Designer.cs
@@ -28,22 +28,41 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Location = new System.Drawing.Point(44, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(603, 294);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(739, 352);
+            this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
             this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- DesenMouse/DesenMouse/Form1.cs
+++ DesenMouse/DesenMouse/Form1.cs
@@ -12,9 +12,44 @@ namespace DesenMouse
 {
     public partial class Form1 : Form
     {
+        int x1, y1, x2, y2;
+        bool deseneaza = false;
+        Graphics desen;
+        Pen creion;
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            deseneaza = true;
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            deseneaza = false;
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            x2 = e.X;
+            y2 = e.Y;
+            deseneazaM();
+            x1 = x2;
+            y1 = y2;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            desen = pictureBox1.CreateGraphics();
+            creion = new Pen(Color.Navy);
+        }
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        void deseneazaM()
+        {
+            if (deseneaza) desen.DrawLine(creion, x1, y1, x2, y2);
         }
 
         private void Form1_MouseMove(object sender, MouseEventArgs e)
@@ -29,8 +64,9 @@ namespace DesenMouse
             x = e.X.ToString();
             y = e.Y.ToString();
             if (e.Button == MouseButtons.Left) buton = "stanga";
-            MessageBox.Show("Ati dat click" + buton + " la coordonatele" + x + " si " + y);
-
+            MessageBox.Show("Ati dat click" + buton + " la coordonatele " + x + " si " + y);
         }
+
+
     }
 }
