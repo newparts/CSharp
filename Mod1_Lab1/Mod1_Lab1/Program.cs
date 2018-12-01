c3951edc912cc3f569c8f04fdf6d1146f1be0840
newparts <iuliu@newparts.ro>
2018-12-01 19:03:30 +0200

    ex
    update

---
+++ Mod1_Lab1/Mod1_Lab1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mod1_Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = "";
+            string lastName = "";
+            int age = 0;
+            string street = "";
+            string city = "";
+            string country = "";
+            DateTime birthDate;
+
+            firstName = "Ion";
+            lastName = "Ionescu";
+            age = 20;
+            street = "Bulevardul 1 Decembrie 1918";
+            city = "Alba Iulia";
+            country = "Romania";
+            birthDate = new DateTime(1990, 6, 28);
+
+            Console.WriteLine(firstName);
+            Console.WriteLine(lastName);
+
+            Console.WriteLine("{0} ani", age);
+
+            Console.WriteLine(street + ", " + city + ", " + country);
+
+            Console.WriteLine($"Nascut in {birthDate}");
+
+            Console.ReadLine();
+        }
+    }
+}
