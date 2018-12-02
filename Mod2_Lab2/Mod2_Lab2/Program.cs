b44b7f6376162efc9b8fe0b8640388642d788e21
newparts <iuliu@newparts.ro>
2018-12-02 23:18:41 +0200

    switch
    final

---
+++ Mod2_Lab2/Mod2_Lab2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mod2_Lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Cafea: 1 = Mica 2 = Medie 3 = Mare");
+            Console.Write("Alegeti o cafea: ");
+            string str = Console.ReadLine();
+            int cost = 0;
+
+            switch (str)
+            {
+                case "1":
+                case "Mica":
+                    cost += 25;
+                    break;
+
+                case "2":
+                case "Medie":
+                    cost += 50;
+                    break;
+
+                case "3":
+                case "Mare":
+                    cost += 75;
+                    break;
+
+                default:
+                    Console.WriteLine("Selectie invalida. Selectati 1, 2 sau 3 !");
+                    break;
+            }
+            if (cost !=0)
+            {
+                Console.WriteLine("Va rugam introduceti {0} lei", cost);
+            }
+            Console.WriteLine("Multumim!");
+            Console.ReadLine();
+        }
+    }
+}
