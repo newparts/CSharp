efefe23574d9ce64745d4ec95949b6363aefe176
newparts <iuliu@newparts.ro>
2018-12-02 23:46:02 +0200

    do while
    final

---
+++ Mod2_Lab4/Mod2_Lab4/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mod2_Lab4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 1;
+
+            while (n<6)
+            {
+                Console.WriteLine($"Valoarea curenta a lui n este {n}");
+                n++;
+            }
+
+            int x = 0;
+
+            do
+            {
+                Console.WriteLine(x);
+                x++;
+            } while (x < 5);
+            Console.ReadLine();
+        }
+    }
+}
