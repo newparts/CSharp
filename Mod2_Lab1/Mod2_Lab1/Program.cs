c6c4983a828e82a96571cf7d32a78ab71d8cefc1
newparts <iuliu@newparts.ro>
2018-12-02 22:52:48 +0200

    if statement
    final

---
+++ Mod2_Lab1/Mod2_Lab1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mod2_Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduceti un numar intreg si apasati tasta Enter");
+            int input = Int32.Parse(Console.ReadLine());
+
+            if (input %2 ==0)
+            {
+                Console.WriteLine("Numarul introdus este par");
+            }
+            else
+            {
+                Console.WriteLine("Numarul introdus este impar");
+            }
+            Console.ReadLine();
+        }
+    }
+}
