f3880e77c6a1e20b0b286b22e8ce98f9e719aec2
newparts <iuliu@newparts.ro>
2018-12-02 22:37:28 +0200

    piramida
    piramida update

--- Grafica/Grafica/Form1.cs
+++ Grafica/Grafica/Form1.cs
@@ -21,9 +21,9 @@ namespace Grafica
         private void pictureBox1_Click(object sender, EventArgs e)
         {
             Thread firgalben = new Thread(()=> deseneaza(Color.Yellow,150,100));
-            Thread firnavy = new Thread(()=> deseneaza(Color.Navy, 100, 150));
+            //Thread firnavy = new Thread(()=> deseneaza(Color.Navy, 100, 150));
             firgalben.Start();
-            firnavy.Start();
+            //firnavy.Start();
         }
 
         private void deseneaza(Color culoare, Int16 Rx, Int16 Ry)
@@ -31,15 +31,31 @@ namespace Grafica
             Graphics g = pictureBox1.CreateGraphics();
             Pen creion = new Pen(culoare);
             Pen guma = new Pen(Color.Yellow);
-            int x, y;
+            int x1, y1, x2, y2, x3, y3, vx=330, vy=100;
             double alpha;
+            g.DrawEllipse(creion, 130, 230, 400, 200);
             for(alpha = -3.14; alpha<=3.14; alpha+=0.02)
             {
-                x = (int)(250 + Rx * Math.Cos(alpha));
-                y = (int)(250 + Ry * Math.Sin(alpha));
-                g.DrawRectangle(creion, x, y, 2, 2);
+                x1 = (int)(330 + Rx * Math.Cos(alpha));
+                y1 = (int)(330 + Ry * Math.Sin(alpha));
+                x2 = (int)(330 + Rx * Math.Cos(alpha+2));
+                y2 = (int)(330 + Ry * Math.Sin(alpha+2));
+                x3 = (int)(330 + Rx * Math.Cos(alpha-2));
+                y3 = (int)(330 + Ry * Math.Sin(alpha-2));
+                g.DrawLine(creion, x1, y1, x2, y2);
+                g.DrawLine(creion, x1, y1, x3, y3);
+                g.DrawLine(creion, x3, y3, x2, y2);
+                g.DrawLine(creion, x1, y1, vx, vy);
+                g.DrawLine(creion, x2, y2, vx, vy);
+                g.DrawLine(creion, x3, y3, vx, vy);
                 Thread.Sleep(15);
-                g.DrawRectangle(guma, x, y, 2, 2);
+                g.DrawLine(guma, x1, y1, x2, y2);
+                g.DrawLine(guma, x1, y1, x3, y3);
+                g.DrawLine(guma, x3, y3, x2, y2);
+                g.DrawLine(guma, x1, y1, vx, vy);
+                g.DrawLine(guma, x2, y2, vx, vy);
+                g.DrawLine(guma, x3, y3, vx, vy);
+                g.DrawEllipse(creion, 130, 230, 400, 200);
             }
         }
     }
