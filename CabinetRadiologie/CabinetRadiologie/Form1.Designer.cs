b762d912091e25f71d2e62b27e1f389c496765fc
newparts <iuliu@newparts.ro>
2018-12-11 16:11:14 +0200

    Cabinet Radiologie
    Some update

---
+++ CabinetRadiologie/CabinetRadiologie/Form1.Designer.cs
@@ -0,0 +1,282 @@
+﻿namespace CabinetRadiologie
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.cabinetRadiologieDataSet = new CabinetRadiologie.CabinetRadiologieDataSet();
+            this.pacientiBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.pacientiTableAdapter = new CabinetRadiologie.CabinetRadiologieDataSetTableAdapters.PacientiTableAdapter();
+            this.tableAdapterManager = new CabinetRadiologie.CabinetRadiologieDataSetTableAdapters.TableAdapterManager();
+            this.radiografiiBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.radiografiiTableAdapter = new CabinetRadiologie.CabinetRadiologieDataSetTableAdapters.RadiografiiTableAdapter();
+            this.pacientiDataGridView = new System.Windows.Forms.DataGridView();
+            this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.radiografiiDataGridView = new System.Windows.Forms.DataGridView();
+            this.dataGridViewTextBoxColumn4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn8 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn9 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.radiografiiDataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridViewTextBoxColumn10 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn11 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn12 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn13 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn14 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn15 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.cabinetRadiologieDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pacientiBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radiografiiBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pacientiDataGridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radiografiiDataGridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radiografiiDataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cabinetRadiologieDataSet
+            // 
+            this.cabinetRadiologieDataSet.DataSetName = "CabinetRadiologieDataSet";
+            this.cabinetRadiologieDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // pacientiBindingSource
+            // 
+            this.pacientiBindingSource.DataMember = "Pacienti";
+            this.pacientiBindingSource.DataSource = this.cabinetRadiologieDataSet;
+            this.pacientiBindingSource.CurrentChanged += new System.EventHandler(this.pacientiBindingSource_CurrentChanged);
+            // 
+            // pacientiTableAdapter
+            // 
+            this.pacientiTableAdapter.ClearBeforeFill = true;
+            // 
+            // tableAdapterManager
+            // 
+            this.tableAdapterManager.BackupDataSetBeforeUpdate = false;
+            this.tableAdapterManager.PacientiTableAdapter = this.pacientiTableAdapter;
+            this.tableAdapterManager.RadiografiiTableAdapter = this.radiografiiTableAdapter;
+            this.tableAdapterManager.UpdateOrder = CabinetRadiologie.CabinetRadiologieDataSetTableAdapters.TableAdapterManager.UpdateOrderOption.InsertUpdateDelete;
+            // 
+            // radiografiiBindingSource
+            // 
+            this.radiografiiBindingSource.DataMember = "FK__Radiografii__CNP__25869641";
+            this.radiografiiBindingSource.DataSource = this.pacientiBindingSource;
+            // 
+            // radiografiiTableAdapter
+            // 
+            this.radiografiiTableAdapter.ClearBeforeFill = true;
+            // 
+            // pacientiDataGridView
+            // 
+            this.pacientiDataGridView.AutoGenerateColumns = false;
+            this.pacientiDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.pacientiDataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dataGridViewTextBoxColumn1,
+            this.dataGridViewTextBoxColumn2,
+            this.dataGridViewTextBoxColumn3});
+            this.pacientiDataGridView.DataSource = this.pacientiBindingSource;
+            this.pacientiDataGridView.Location = new System.Drawing.Point(12, 12);
+            this.pacientiDataGridView.Name = "pacientiDataGridView";
+            this.pacientiDataGridView.Size = new System.Drawing.Size(300, 220);
+            this.pacientiDataGridView.TabIndex = 0;
+            // 
+            // dataGridViewTextBoxColumn1
+            // 
+            this.dataGridViewTextBoxColumn1.DataPropertyName = "CNP";
+            this.dataGridViewTextBoxColumn1.HeaderText = "CNP";
+            this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
+            // 
+            // dataGridViewTextBoxColumn2
+            // 
+            this.dataGridViewTextBoxColumn2.DataPropertyName = "Nume";
+            this.dataGridViewTextBoxColumn2.HeaderText = "Nume";
+            this.dataGridViewTextBoxColumn2.Name = "dataGridViewTextBoxColumn2";
+            // 
+            // dataGridViewTextBoxColumn3
+            // 
+            this.dataGridViewTextBoxColumn3.DataPropertyName = "Adresa";
+            this.dataGridViewTextBoxColumn3.HeaderText = "Adresa";
+            this.dataGridViewTextBoxColumn3.Name = "dataGridViewTextBoxColumn3";
+            // 
+            // radiografiiDataGridView
+            // 
+            this.radiografiiDataGridView.AutoGenerateColumns = false;
+            this.radiografiiDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.radiografiiDataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dataGridViewTextBoxColumn4,
+            this.dataGridViewTextBoxColumn5,
+            this.dataGridViewTextBoxColumn6,
+            this.dataGridViewTextBoxColumn7,
+            this.dataGridViewTextBoxColumn8,
+            this.dataGridViewTextBoxColumn9});
+            this.radiografiiDataGridView.DataSource = this.radiografiiBindingSource;
+            this.radiografiiDataGridView.Location = new System.Drawing.Point(12, 238);
+            this.radiografiiDataGridView.Name = "radiografiiDataGridView";
+            this.radiografiiDataGridView.Size = new System.Drawing.Size(300, 220);
+            this.radiografiiDataGridView.TabIndex = 1;
+            // 
+            // dataGridViewTextBoxColumn4
+            // 
+            this.dataGridViewTextBoxColumn4.DataPropertyName = "Id";
+            this.dataGridViewTextBoxColumn4.HeaderText = "Id";
+            this.dataGridViewTextBoxColumn4.Name = "dataGridViewTextBoxColumn4";
+            // 
+            // dataGridViewTextBoxColumn5
+            // 
+            this.dataGridViewTextBoxColumn5.DataPropertyName = "CNP";
+            this.dataGridViewTextBoxColumn5.HeaderText = "CNP";
+            this.dataGridViewTextBoxColumn5.Name = "dataGridViewTextBoxColumn5";
+            // 
+            // dataGridViewTextBoxColumn6
+            // 
+            this.dataGridViewTextBoxColumn6.DataPropertyName = "Imagine";
+            this.dataGridViewTextBoxColumn6.HeaderText = "Imagine";
+            this.dataGridViewTextBoxColumn6.Name = "dataGridViewTextBoxColumn6";
+            // 
+            // dataGridViewTextBoxColumn7
+            // 
+            this.dataGridViewTextBoxColumn7.DataPropertyName = "Data";
+            this.dataGridViewTextBoxColumn7.HeaderText = "Data";
+            this.dataGridViewTextBoxColumn7.Name = "dataGridViewTextBoxColumn7";
+            // 
+            // dataGridViewTextBoxColumn8
+            // 
+            this.dataGridViewTextBoxColumn8.DataPropertyName = "Diagnostic";
+            this.dataGridViewTextBoxColumn8.HeaderText = "Diagnostic";
+            this.dataGridViewTextBoxColumn8.Name = "dataGridViewTextBoxColumn8";
+            // 
+            // dataGridViewTextBoxColumn9
+            // 
+            this.dataGridViewTextBoxColumn9.DataPropertyName = "Comentarii";
+            this.dataGridViewTextBoxColumn9.HeaderText = "Comentarii";
+            this.dataGridViewTextBoxColumn9.Name = "dataGridViewTextBoxColumn9";
+            // 
+            // radiografiiDataGridView1
+            // 
+            this.radiografiiDataGridView1.AutoGenerateColumns = false;
+            this.radiografiiDataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.radiografiiDataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dataGridViewTextBoxColumn10,
+            this.dataGridViewTextBoxColumn11,
+            this.dataGridViewTextBoxColumn12,
+            this.dataGridViewTextBoxColumn13,
+            this.dataGridViewTextBoxColumn14,
+            this.dataGridViewTextBoxColumn15});
+            this.radiografiiDataGridView1.DataSource = this.radiografiiBindingSource;
+            this.radiografiiDataGridView1.Location = new System.Drawing.Point(363, 12);
+            this.radiografiiDataGridView1.Name = "radiografiiDataGridView1";
+            this.radiografiiDataGridView1.Size = new System.Drawing.Size(300, 220);
+            this.radiografiiDataGridView1.TabIndex = 2;
+            // 
+            // dataGridViewTextBoxColumn10
+            // 
+            this.dataGridViewTextBoxColumn10.DataPropertyName = "Id";
+            this.dataGridViewTextBoxColumn10.HeaderText = "Id";
+            this.dataGridViewTextBoxColumn10.Name = "dataGridViewTextBoxColumn10";
+            // 
+            // dataGridViewTextBoxColumn11
+            // 
+            this.dataGridViewTextBoxColumn11.DataPropertyName = "CNP";
+            this.dataGridViewTextBoxColumn11.HeaderText = "CNP";
+            this.dataGridViewTextBoxColumn11.Name = "dataGridViewTextBoxColumn11";
+            // 
+            // dataGridViewTextBoxColumn12
+            // 
+            this.dataGridViewTextBoxColumn12.DataPropertyName = "Imagine";
+            this.dataGridViewTextBoxColumn12.HeaderText = "Imagine";
+            this.dataGridViewTextBoxColumn12.Name = "dataGridViewTextBoxColumn12";
+            // 
+            // dataGridViewTextBoxColumn13
+            // 
+            this.dataGridViewTextBoxColumn13.DataPropertyName = "Data";
+            this.dataGridViewTextBoxColumn13.HeaderText = "Data";
+            this.dataGridViewTextBoxColumn13.Name = "dataGridViewTextBoxColumn13";
+            // 
+            // dataGridViewTextBoxColumn14
+            // 
+            this.dataGridViewTextBoxColumn14.DataPropertyName = "Diagnostic";
+            this.dataGridViewTextBoxColumn14.HeaderText = "Diagnostic";
+            this.dataGridViewTextBoxColumn14.Name = "dataGridViewTextBoxColumn14";
+            // 
+            // dataGridViewTextBoxColumn15
+            // 
+            this.dataGridViewTextBoxColumn15.DataPropertyName = "Comentarii";
+            this.dataGridViewTextBoxColumn15.HeaderText = "Comentarii";
+            this.dataGridViewTextBoxColumn15.Name = "dataGridViewTextBoxColumn15";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 598);
+            this.Controls.Add(this.radiografiiDataGridView1);
+            this.Controls.Add(this.radiografiiDataGridView);
+            this.Controls.Add(this.pacientiDataGridView);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.cabinetRadiologieDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pacientiBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radiografiiBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pacientiDataGridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radiografiiDataGridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radiografiiDataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private CabinetRadiologieDataSet cabinetRadiologieDataSet;
+        private System.Windows.Forms.BindingSource pacientiBindingSource;
+        private CabinetRadiologieDataSetTableAdapters.PacientiTableAdapter pacientiTableAdapter;
+        private CabinetRadiologieDataSetTableAdapters.TableAdapterManager tableAdapterManager;
+        private CabinetRadiologieDataSetTableAdapters.RadiografiiTableAdapter radiografiiTableAdapter;
+        private System.Windows.Forms.BindingSource radiografiiBindingSource;
+        private System.Windows.Forms.DataGridView pacientiDataGridView;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn3;
+        private System.Windows.Forms.DataGridView radiografiiDataGridView;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn7;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn8;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn9;
+        private System.Windows.Forms.DataGridView radiografiiDataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn10;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn11;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn12;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn13;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn14;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn15;
+    }
+}
+
---
+++ CabinetRadiologie/CabinetRadiologie/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CabinetRadiologie
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pacientiBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.pacientiBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.cabinetRadiologieDataSet);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'cabinetRadiologieDataSet.Radiografii' table. You can move, or remove it, as needed.
+            this.radiografiiTableAdapter.Fill(this.cabinetRadiologieDataSet.Radiografii);
+            // TODO: This line of code loads data into the 'cabinetRadiologieDataSet.Pacienti' table. You can move, or remove it, as needed.
+            this.pacientiTableAdapter.Fill(this.cabinetRadiologieDataSet.Pacienti);
+
+        }
+
+        private void pacientiBindingSource_CurrentChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
