42b5b9d33cccf2e1084da76bac115dea23a4bfb8
newparts <iuliu@newparts.ro>
2018-12-20 13:21:45 +0200

    Chess Table
    Final

---
+++ ChessTable/ChessTable/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int col = 0; col < 8; col++)
+            {
+                for (int row = 0; row < 8; row++)
+                {
+                    if ((col + row) % 2 == 0)
+                    {
+                        Console.Write("X");
+                    }
+                    else if((col + row) % 2 == 1)
+                    {
+                        Console.Write("O");
+                    }
+                    else
+                    {
+                        Console.Write("Neither X nor O");
+                    }
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+            Console.Read();
+        }
+    }
+}
