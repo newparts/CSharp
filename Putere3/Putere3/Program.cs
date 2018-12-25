9fd7aa73dc778cad79e3d90b7eaa5b722460f20e
newparts <iuliu@newparts.ro>
2018-12-25 14:16:32 +0200

    putere3
    final

---
+++ Putere3/Putere3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Putere3
+{
+    class Putere3
+    {
+        public int this[int index]
+        {
+            get
+            {
+                if (index >= 0)
+                    return (int)Math.Pow(3, index);
+                else
+                    return -1;
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Putere3 p = new Putere3();
+            for (int i = 0; i < 20; i++)
+                Console.WriteLine("3^{0}={1}", i, p[i]);
+            Console.Read();
+        }
+    }
+}
