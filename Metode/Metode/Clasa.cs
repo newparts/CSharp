a51449f3e152b3e47f60cdd39387e4d117b1368e
newparts <iuliu@newparts.ro>
2019-01-12 22:00:41 +0200

    final
    final

---
+++ Metode/Metode/Clasa.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metode
+{
+    class Clasa
+    {
+        public void whoAmI(string name)
+        {
+            Console.WriteLine("Hello " + name + ", how are you doing?");
+        }
+
+        public int addMe(int a, int b)
+        {
+            int totalValue;
+            totalValue = (a + b);
+            return totalValue;
+        }
+
+        public int addMe(int a, int b, int c)
+        {
+            int totalValue;
+            totalValue = (a + b + c);
+            return totalValue;
+        }
+    }
+}
---
+++ Metode/Metode/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Metode;
+
+namespace Metode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Metode.Clasa exMeth = new Clasa();
+            exMeth.whoAmI("Gigi");
+            Console.WriteLine(exMeth.addMe(3, 8));
+            Console.WriteLine(exMeth.addMe(3, 8, 4));
+            Console.ReadLine();
+        }
+    }
+}
