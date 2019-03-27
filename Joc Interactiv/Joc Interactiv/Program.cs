8893feffffe36d71407c4f336835a51d2095718f
newparts <iuliu@newparts.ro>
2019-03-27 23:57:30 +0200

    update
    functioneaza

--- Joc Interactiv/Joc Interactiv/Program.cs
+++ Joc Interactiv/Joc Interactiv/Program.cs
@@ -26,7 +26,70 @@ namespace Joc_Interactiv
 
             Console.WriteLine("Salut {0}, hai sa ne jucam !", inputNume);
 
-            Console.Read();
+            while (true)
+            {
+                Random random = new Random();
+
+                int numarCorect = random.Next(1, 10);
+
+                int aleator = 0;
+
+                Console.WriteLine("Ghiceste numarul intre 1 si 10");
+
+                while (aleator != numarCorect)
+                {
+                    string input = Console.ReadLine();
+
+                    if (!int.TryParse(input, out aleator))
+                    {
+                        Console.ForegroundColor = ConsoleColor.Blue;
+
+                        Console.WriteLine("Te rog tasteaza un numar !");
+
+                        Console.ResetColor();
+
+                        continue;
+                    }
+
+                    aleator = Int32.Parse(input);
+
+                    if (aleator != numarCorect)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+
+                        Console.WriteLine("Numar gresit, incearca din nou !");
+
+                        Console.ResetColor();
+                    }
+                }
+
+                Console.ForegroundColor = ConsoleColor.Green;
+
+                Console.WriteLine("Ai ghicit numarul corect !");
+
+                Console.ResetColor();
+
+                Console.WriteLine("Joaca din nou? [D sau N]");
+
+                string raspuns = Console.ReadLine().ToUpper();
+
+                if(raspuns == "D")
+                {
+                    continue;
+                }
+                else if(raspuns == "N")
+                {
+                    return;
+                }
+                else
+                {
+                    return;
+                }
+            
+                Console.Read();
+
+
+            }
         }
     }
 }
